0de69bcb403d8c10f0c3470eb07c0e682f1efa6f
umarbek-backend-engineer <259284488+umarbek-x@users.noreply.github.com>
2026-05-02 16:40:57 +0500

    fixed error in migrations in services like music and lyrics

--- lyrics-service/migrator/migrations/1_create_lyrics_table.up.sql
+++ lyrics-service/migrator/migrations/1_create_lyrics_table.up.sql
@@ -1,12 +1,9 @@
 create table lyrics (
     id uuid primary key default gen_random_uuid(),
-    music_id uuid not null references music(id) on delete cascade,
+    music_id uuid not null,
     title text not null,
     content JSONB not null,
     is_verified boolean default false,
-    created_at timestamp default now(),
-    updated_at timestamp default now()
+    created_at timestamptz default now(),
+    updated_at timestamptz default now()
 );
-
-create index idx_lyrics_music_id on lyrics(music_id);
-create index idx_lyrics_user_id on lyrics(user_id);
--- music-service/migrator/migrations/1_create_music_table.up.sql
+++ music-service/migrator/migrations/1_create_music_table.up.sql
@@ -1,10 +1,8 @@
 create table music (
     id uuid primary key default gen_random_uuid(),
-    user_id uuid not null references users(id) on delete cascade,
+    user_id uuid not null,
     title text not null,
-    filepath text unique not null,
+    filepath text not null,
     is_public boolean default false,
-    uploaded_at timestamp default now()
+    uploaded_at timestamptz default now()
 );
-
-create index idx_music_user_id on music(user_id);
